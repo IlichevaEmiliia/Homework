e45a911753b8c4ee1ef0514d132fb7c44f55e5b9
IlichevaEmiliia <100027795+IlichevaEmiliia@users.noreply.github.com>
2022-03-22 22:03:25 +0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -10,44 +10,59 @@ directories = {
 }
 
 
-def p(num): #узнать владельца по его номеру
+def poisk_doc(num): #найти владельца документа по номеру
     for el in documents:
         if num == el['number']:
             return el['name']
 
 
-def s(num): #узнать по номеру на какой полке хранится документ
+def search_shelf(num): #найти на какой полке документ
     for k, v in directories.items():
         if num in v:
             return k
 
 
-def full_information(): #распечатать полную информацию о документах
+def p(num):
+    if poisk_doc(num):
+        return f"Владелец документа: {poisk_doc(num)}"
+    else:
+        return 'Документ не найден в базе'
+
+
+def s(num):
+    if search_shelf(num):
+        return f"Документ хранится на полке: {search_shelf(num)}"
+    else:
+        return 'Документ не найден в базе'
+
+
+def full_information():  # распечатать полную информацию о документах
     for el in documents:
         print(f"№: {el['number']}, тип: {el['type']}, владелец: {el['name']}, полка хранения: {s(el['number'])}")
 
 
-def ads(num): #добавить полку
-    directories[num] = []
-    return f"Полка добавлена. Текущий перечень полок: {', '.join(list(directories.keys()))}."
+def ads(num):  # добавить полку
+    if num in directories.keys():
+        return f"Такая полка уже существует. Текущий перечень полок: {', '.join(list(directories.keys()))}."
+    else:
+        directories[num] = []
+        return f"Полка добавлена. Текущий перечень полок: {', '.join(list(directories.keys()))}."
 
 
-def ds(num): #удалить полку
+def ds(num):  # удалить полку
     if num in directories.keys():
         if len(directories[num]) == 0:
             del directories[num]
-            return f'Полка удалена. Текущий перечень полок: {", ".join(list(directories.keys()))}'
+            return f"Полка удалена. Текущий перечень полок: {', '.join(list(directories.keys()))}."
         else:
-            return f'На полке есть документы, удалите их перед удалением полки. Текущий перечень полок: {", ".join(list(directories.keys()))}'
+            return f"На полке есть документы, удалите их перед удалением полки. Текущий перечень полок: {', '.join(list(directories.keys()))}."
     else:
-        return f'Такой полки не существует. Текущий перечень полок: {", ".join(list(directories.keys()))}'
-    
-    
-def ad(num): #добавить документ
-    new_doc = {}
-    new_doc['number'] = input('Введите номер документа: \n')
-    new_doc['type'] = input('Введите тип документа: \n')
-    new_doc['name'] = input('Введите владельца документа: \n')
+        return f"Такой полки не существует. Текущий перечень полок: {', '.join(list(directories.keys()))}."
+
+
+def ad():  # добавить документ
+    new_doc = {'number': input('Введите номер документа: \n'), 'type': input('Введите тип документа: \n'),
+               'name': input('Введите владельца документа: \n')}
     shelf = input('Введите полку для хранения: \n')
     if shelf in directories.keys():
         documents.append(new_doc)
@@ -58,34 +73,29 @@ def ad(num): #добавить документ
         print('Такой полки не существует. Вы можете добавить новую полку командой ads. \nТекущий список документов:')
         full_information()
 
-        
-def d(num): #удалить документ
-    name = p(num)
-    if name:
-        directories[s(num)].remove(num)
-        documents.remove(i)
-        print('Документ удалён. \nТекщий список документов:')
-        full_information()
-    else:
-        print('Документ не найден в базе. \nТекущий список документов:')
-        full_information()
-        
-        
-def m(): #переместить документ с полки на полку
+
+def d(num):  # удалить документ
+    for i in range(len(documents)):
+        if documents[i]["number"] == num:
+            documents.pop(i)
+            return 'Документ удалён. \nТекщий список документов:'
+    return 'Документ не найден в базе. \nТекущий список документов:'
+
+
+def m():  # переместить документ с полки на полку
     num = input('Введите номер документа: \n')
     shelf = input('Введите номер полки: \n')
-    if p(num):
-        if shelf in directories.keys():
-            directories[s(num)].remove(num)
-            directories[shelf].append(num)
-            print('Документ перемещен. \nТекущий список документов:')
-            print_all()
-        else:
-            print(f'Такой полки не существует. Текущий перечень полок: {", ".join(list(directories.keys()))}')
-    else:
-        print('Документ не найден в базе. \nТекущий список документов:')
-        
-        
+    for i in range(len(documents)):
+        if documents[i]["number"] == num:
+            if shelf in directories.keys():
+                directories[search_shelf(num)].remove(num)
+                directories[shelf].append(num)
+                return 'Документ перемещен. \nТекущий список документов:'
+            else:
+                return f"Такой полки не существует. Текущий перечень полок: {', '.join(list(directories.keys()))}"
+    return 'Документ не найден в базе. \nТекущий список документов:'
+
+
 while True:
     c = input('Введите команду: ')
     if c == 'q':
@@ -93,7 +103,7 @@ while True:
     elif c == 'p':
         print(p(input('Введите номер документа: ')))
     elif c == 's':
-        print(f"Документ хранится на полке: {s(input('Введите номер документа: '))}")
+        print(s(input('Введите номер документа: ')))
     elif c == 'l':
         full_information()
     elif c == 'ads':
@@ -103,6 +113,8 @@ while True:
     elif c == 'ad':
         ad()
     elif c == 'd':
-        d()
+        print(d(input('Введите номер документа: ')))
+        full_information()
     elif c == 'm':
-        m()
+        print(m())
+        full_information()
